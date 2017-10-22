c1fc9825cbcdc6ace11a3cddc053dfa116742b04
foule <foulegold@gmail.com>
2017-10-23 01:27:34 +0700

    активация, статус, новый юзер

--- index.js
+++ index.js
@@ -24,13 +24,76 @@ mongoClient.connect("mongodb://localhost:27017/betbot", function(err, db){
         if (message.text && message.text.toLowerCase() === '/menu') {
             sendMenuMessage(message);
         }
+        // активация ключа
         if (message.text && callback[message.chat.id] && callback[message.chat.id] === 'activationCmd'){
-            db.collection("users").find({name: 'foule'}).toArray(function(err, results) {
+            db.collection("users").findOne({key : message.text}, function(err, results) {
                 console.log(results);
+                if (results){
+                    let tl;
+                    if (results.month){
+                        tl = 730 * results.month;
+                    }
+                    else {
+                        tl = 730
+                    }
+                    if (results.timeleft) {
+                        if (results.timeleft > 0) {
+                            db.collection("users").findOneAndUpdate({chatid: message.chat.id}, {$set: {timeleft: results.timeleft + tl}}
+                                , function (err, results) {
+                                    tg.sendMessage(message.chat.id, 'Ваш ключ успешно обновлен!');
+                                });
+                            db.collection("users").findOneAndUpdate({chatid: message.chat.id}, {$unset: {key: message.text}}, function (err, results){
+                                console.log('ключ удален');
+                            });
+                        } else {
+                            db.collection("users").findOneAndUpdate({chatid: message.chat.id}, {$set: {timeleft: tl}}
+                                , function (err, results) {
+                                    tg.sendMessage(message.chat.id, 'Ваш ключ успешно активирован! Скоро вам придет первый прогноз!');
+                                });
+                            db.collection("users").findOneAndUpdate({chatid: message.chat.id}, {$unset: {key: message.text}}, function (err, results) {
+                                console.log('ключ удален');
+                            });
+                        }
+                    }else{
+                        db.collection("users").findOneAndUpdate({key: message.text}, {$set: {chatid: message.chat.id, timeleft: tl}}
+                            , function (err, results) {
+                                tg.sendMessage(message.chat.id, 'Ваш ключ успешно активирован! Скоро вам придет первый прогноз!');
+                            });
+                        db.collection("users").findOneAndUpdate({key: message.text}, {$unset: {key: message.text}}, function (err, results) {
+                            console.log('ключ удален');
+                        });
+                    }
+                }else{
+                    tg.sendMessage(message.chat.id, 'Ключ не обнаружен!');
+                }
             });
-            // tg.sendMessage(message.chat.id, 'попал в активацию');
-            console.log('попал в активацию');
             delete callback[message.chat.id];
+        }
+        // заполнение нового юзера
+        if (message.text && callback[message.chat.id] === 'newkeyCmd') {
+            db.collection("users").findOneAndUpdate({name: callback.name}, {$set: {key: message.text}}, function(err, results){
+                if (results) {
+                    console.log(results);
+                    tg.sendMessage(message.chat.id, 'Ключ сохранен');
+                }else {
+                    tg.sendMessage(message.chat.id, 'Пользователь не найден, попробуйте снова.');
+                }
+            });
+            delete callback[message.chat.id];
+            delete callback.name;
+        }
+        if (message.text && callback[message.chat.id] === 'newuserCmd') {
+            db.collection("users").findOne({name : message.text}, function(err, results) {
+                if (results){
+                    tg.sendMessage(message.chat.id, 'Такое имя уже есть в базе!');
+                } else {
+                    db.collection("users").insertOne({name: message.text}, function(err, results){
+                        tg.sendMessage(message.chat.id, 'Имя добавлено. Введите ключ:');
+                    });
+                }
+            });
+            callback[message.chat.id] = 'newkeyCmd';
+            callback.name = message.text;
         }
     }
     function onCallbackQuery(callbackQuery) {
@@ -42,8 +105,6 @@ mongoClient.connect("mongodb://localhost:27017/betbot", function(err, db){
                 break;
             case 'statusCmd' :
                 statuscheck(callbackQuery.message.chat.id);
-                let statusText = "Проверка статуса подписки";
-                tg.sendMessage(callbackQuery.message.chat.id, statusText);
                 tg.answerCallbackQuery(callbackQuery.id);
                 break;
             case 'instructionCmd' :
@@ -52,10 +113,18 @@ mongoClient.connect("mongodb://localhost:27017/betbot", function(err, db){
             case 'faqCmd' :
                 tg.answerCallbackQuery(callbackQuery.id);
                 break;
+            case 'vkCmd' :
+                tg.answerCallbackQuery(callbackQuery.id);
+                break;
+            case 'newuserCmd' :
+                tg.sendMessage(callbackQuery.message.chat.id, 'Введите имя:');
+                tg.answerCallbackQuery(callbackQuery.id);
+                break;
         }
         let id = callbackQuery.message.chat.id;
         callback[id] = callbackQuery.data;
     }
+    // основное меню
     function sendMenuMessage(message) {
         const text = 'Выбери, что тебе нужно';
         const activationButton = {
@@ -76,16 +145,57 @@ mongoClient.connect("mongodb://localhost:27017/betbot", function(err, db){
             callback_data:'faqCmd',
             url: 'http://telegra.ph/FAQ-10-21'
         };
-        const options = {};
-        options.reply_markup = {};
-        options.reply_markup.inline_keyboard = [];
-        options.reply_markup.inline_keyboard.push([activationButton, statusButton]);
-        options.reply_markup.inline_keyboard.push([instructionButton, faqButton]);
+        const vkButton = {
+            text:"Группа ВК",
+            callback_data:'vkCmd',
+            url: 'https://vk.com/betbotcapper'
+        };
+        const newuserButton = {
+            text:"Новый юзер",
+            callback_data:'newuserCmd',
+        };
+        let options;
+        if (message.chat.id === 347860214 || message.chat.id === 264251601){
+            options = {
+                reply_markup: {
+                    inline_keyboard: [
+                        [activationButton, statusButton],
+                        [instructionButton, faqButton, vkButton],
+                        [newuserButton]
+                    ]
+                }
+            };
+        }else {
+            options = {
+                reply_markup: {
+                    inline_keyboard: [
+                        [activationButton, statusButton],
+                        [instructionButton, faqButton, vkButton]
+                    ]
+                }
+            };
+        }
         tg.sendMessage(message.chat.id, text, options);
     }
-    createBot();
-
+    // проверка статуса подписки
     function statuscheck(chatid) {
-
+        // let a = 12;
+        // db.collection("users").findOneAndUpdate({name: 'seclace'}, {$set: {month: a, timeleft: 0, key: "22222"}}, function(err, result){
+        //     console.log(result)});
+        db.collection("users").findOne({chatid: chatid}, function(err, results) {
+            if (results) {
+                let days, hours;
+                if (results.timeleft > 24) {
+                    days = Math.floor(results.timeleft/24);
+                    hours = results.timeleft%24;
+                    tg.sendMessage(chatid, 'Осталось ' + days + 'd ' + hours + 'h.');
+                }else {
+                    tg.sendMessage(chatid, 'Осталось ' + results.timeleft + 'h.');
+                }
+            }else{
+                tg.sendMessage('Вы еще не активировали Киру.');
+            }
+        });
     }
+    createBot();
 });
